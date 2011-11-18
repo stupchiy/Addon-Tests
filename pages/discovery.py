2e57f99c0355bac738836a287b8ac59120aab60c
Zac <zac@zac-Ubuntu.(none)>
2011-11-18 11:50:39 +0000

    Wrong array index for persona list

--- pages/discovery.py
+++ pages/discovery.py
@@ -102,10 +102,10 @@ class DiscoveryPane(Base):
 
     @property
     def first_persona(self):
-        return self.selenium.find_elements(*self._personas_locator)[1].text
+        return self.selenium.find_elements(*self._personas_locator)[0].text
 
     def click_on_first_persona(self):
-        self.selenium.find_elements(*self._personas_locator)[1].click()
+        self.selenium.find_elements(*self._personas_locator)[0].click()
         return DiscoveryPersonasDetail(self.testsetup)
 
     @property
