1240d592e277de9039ab6ca5db3568ca2a084109
bebef1987 <florin.strugariu@softvision.ro>
2011-11-23 18:15:48 +0200

    update

--- tests/test_details_page_against_xml.py
+++ tests/test_details_page_against_xml.py
@@ -114,7 +114,7 @@ class TestDetailsAgainstXML:
 
         Assert.equal(browser_description.replace('\n', ''), xml_description.replace('\n', ''))
 
-    @xfail(reason="needs to be updated for impala")
+    @xfail(reason="https://www.pivotaltracker.com/story/show/17471931")
     def test_that_icon_is_correct(self, mozwebqa):
         """litmus 15322"""
 
